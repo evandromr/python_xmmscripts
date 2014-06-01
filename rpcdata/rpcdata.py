0d53fc88d703d6c79a11b45b2fa6d861c0e213e9
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-06-01 10:58:42 -0300

    start transition to python3

--- rpcdata/rpcdata.py
+++ rpcdata/rpcdata.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Script to reprocess observation data files
 import subprocess
@@ -17,27 +17,26 @@ os.environ['SAS_VERBOSITY'] = '0'
 os.environ['XPA_METHOD'] = 'local'
 # =================================================================
 
-
 #Point SAS_ODF to the raw observation data directory
 os.environ['SAS_ODF'] = odffolder
 
-print 'Building calibration files index...'
+print('Building calibration files index...')
 subprocess.call(['cifbuild'])
 
 #Point to the calibration index
 os.environ['SAS_CCF'] = os.path.abspath('ccf.cif')
 
 #Create observation summary
-print 'Creating observation summary...'
+print('Creating observation summary...')
 subprocess.call(['odfingest'])
 
 #Point to the Summary file
 os.environ['SAS_ODF'] = os.path.abspath(glob.glob('*SUM.SAS')[0])
 
 #Reprocess data
-print 'Running epproc...'
+print('Running epproc...')
 subprocess.call(['epproc'])  # PN camera
-print 'Running emproc...'
+print('Running emproc...')
 subprocess.call(['emproc'])  # MOS cameras
 #(one can use "(ep/em)chain" instead, see the SAS documentation)
 
@@ -45,14 +44,10 @@ shutil.copyfile(glob.glob('*PN*ImagingEvts.ds')[0], 'pnevents.ds')
 shutil.copyfile(glob.glob('*MOS1*ImagingEvts.ds')[0], 'm1events.ds')
 shutil.copyfile(glob.glob('*MOS2*ImagingEvts.ds')[0], 'm2events.ds')
 
-shutil.copyfile('pnevents.ds', 'pnevents_barycen.ds')
-shutil.copyfile('m1events.ds', 'm1events_barycen.ds')
-shutil.copyfile('m2events.ds', 'm2events_barycen.ds')
+#Check everything
+subprocess.call(['sasversion'])
 
-subprocess.call(['barycen', 'table=pnevents_barycen.ds:EVENTS'])
-subprocess.call(['barycen', 'table=m1events_barycen.ds:EVENTS'])
-subprocess.call(['barycen', 'table=m2events_barycen.ds:EVENTS'])
-
+# Writes observation times to file times.dat
 timefile = open('times.dat', 'w')
 
 tstarts = [fits.getval('pnevents_barycen.ds', 'TSTART', extname='EVENTS'),
@@ -77,6 +72,3 @@ timefile.write('tstop={0} \n'.format(min(tstops)))
 timefile.write('duration={0}'.format(min(tstops)-max(tstarts)))
 
 timefile.close()
-
-#Check everything
-subprocess.call(['sasversion'])
