12143340b3d2bd016f7d669dcdeb86967c326331
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-07-02 10:42:27 -0300

    remove suffix from the main event file

--- pn/spectra/pnspec.py
+++ pn/spectra/pnspec.py
@@ -17,7 +17,7 @@ bkgregionfile = os.path.abspath('bkg.reg')
 prefix = ""
 sufix = ""
 
-table = 'pn_clean{1}.ds'.format(prefix, sufix)
+table = 'pn_clean.ds'
 srcspc = '{0}pn_srcspc{1}.ds'.format(prefix, sufix)
 srcimg = '{0}pn_srcimg{1}.ds'.format(prefix, sufix)
 bkgspc = '{0}pn_bkgspc{1}.ds'.format(prefix, sufix)
